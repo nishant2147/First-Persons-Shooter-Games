44bee385d6e513805ed577c09becb982c7fbd848
nishant2147 <bishtuma147@gmail.com>
2024-07-20 16:35:17 +0530

    Update PlayerMovement

--- Script/PlayerMovement.cs
+++ Script/PlayerMovement.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //This is Test Msg
     [SerializeField] float RotationSpeed;
     [SerializeField] float Speed;
     CharacterController controller;
